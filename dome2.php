0d45ba20546e244d855a876de8580e2c78e0824a
魑魅 <58153186+zhanghaoaaa@users.noreply.github.com>
2019-12-11 09:12:44 +0800

    Add files via upload

---
+++ dome2.php
@@ -0,0 +1,21 @@
+
+
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body>
+  
+<?php
+	echo "成功！"
+
+	?>
+<a href="index.html">跳转</a>
+ </body>
+</html>
